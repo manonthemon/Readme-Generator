8a84d1e372d101fba45be63de721168294e82762
Manonthemon <klimowicz.maciej@gmail.com>
2023-02-17 10:34:34 +0000

    Practicing using argv

--- index.js
+++ index.js
@@ -3,6 +3,26 @@ const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
+console.log(process.argv);
+
+
+console.log(`The first element in the argument array is ${process.argv[0]}`);
+console.log(`The second element in the argument array is ${process.argv[1]}`);
+console.log(`The third element in the argument array is ${process.argv[2]}`);
+console.log(`The fourth element in the argument array is ${process.argv[3]}`);
+
+const name = process.argv[2]
+const iterations = process.argv[3]
+
+console.log(`We are going to print ${name} ${iterations} times`);
+
+for (let i =0; i < iterations; i++) {
+    console.log(`Hello, ${name}`);
+}
+
+console.log(`The length of the process array is ${process.argv.length}`);
+
+
 //EXAMPLE INQUIRER CODE
 // inquirer
 //   .prompt([
