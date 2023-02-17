4985a77122d080aaab0f18f917e2550339d61735
Manonthemon <klimowicz.maciej@gmail.com>
2023-02-17 09:54:12 +0000

    Added example inquirer code for further developement

--- index.js
+++ index.js
@@ -3,6 +3,33 @@ const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
+//EXAMPLE INQUIRER CODE
+// inquirer
+//   .prompt([
+//     {
+//       type: 'input',
+//       message: 'What is your user name?',
+//       name: 'username',
+//     },
+//     {
+//       type: 'password',
+//       message: 'What is your password?',
+//       name: 'password',
+//     },
+//     {
+//       type: 'password',
+//       message: 'Re-enter password to confirm:',
+//       name: 'confirm',
+//     },
+//   ])
+//   .then((response) =>
+//     response.confirm === response.password
+//       ? console.log('Success!')
+//       : console.log('You forgot your password already?!')
+//   );
+
+
+
 
 // array of questions for user
 const questions = [
