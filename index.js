ecf3377adbce24d4e45a609316c8a59bd976f82f
Manonthemon <klimowicz.maciej@gmail.com>
2023-02-17 10:48:15 +0000

    Added comments

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
+
+// Required packages
+
 const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
+// Prints the array of process to console
 console.log(process.argv);
 
+// Reads another file, in this case the generateMarkdown file
+fs.readFile('./utils/generateMarkdown.js' , 'utf8', (error, data) => 
+error ? console.error(error) : console.log(data)
+);
 
+//Logs consecutive elements of process argv
 console.log(`The first element in the argument array is ${process.argv[0]}`);
 console.log(`The second element in the argument array is ${process.argv[1]}`);
 console.log(`The third element in the argument array is ${process.argv[2]}`);
 console.log(`The fourth element in the argument array is ${process.argv[3]}`);
 
+
+//Assigns elements of process argv to variables
 const name = process.argv[2]
 const iterations = process.argv[3]
 
 console.log(`We are going to print ${name} ${iterations} times`);
 
+//creates a loop to print 3-rd elements of process a number of times specified in 4-th element of process
+
 for (let i =0; i < iterations; i++) {
     console.log(`Hello, ${name}`);
 }
 
+//prints length of process argv to console
 console.log(`The length of the process array is ${process.argv.length}`);
 
 
